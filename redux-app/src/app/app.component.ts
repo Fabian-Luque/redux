5945b82efc0f94513bc71a9b4a780e5ebcd55c8b
fabian <fabianluque2014@gmail.com>
2022-04-18 10:26:14 -0400

    first commit

---
+++ redux-app/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as actions from './contador/contador.actions';
+
+import { AppState } from "./app.reducers";
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'redux-app';
+  contador!: number;
+
+  constructor( private store : Store<AppState>){
+    
+    this.store.select('contador').subscribe( contador => this.contador = contador);
+
+  }
+
+  incrementar = () => {
+
+    this.store.dispatch( actions.incrementar() );
+
+  }
+    // this.contador++;
+
+  decrementar = () => {
+    this.store.dispatch( actions.decrementar() );
+
+  }
+    // this.contador--;
+}
---
+++ todo-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { StoreModule } from '@ngrx/store';
+import { todoReducer } from './todos/todo.reducer';
+
+import { AppComponent } from './app.component';
+import { TodoModule } from './todos/todo.module';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    TodoModule,
+    StoreModule.forRoot({ todos: todoReducer})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
